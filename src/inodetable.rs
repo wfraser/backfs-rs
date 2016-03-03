31124a04945236eca45c4802a27edc059420ed9f
William R. Fraser <wfraser@codewise.org>
2016-03-02 21:27:18 -0800

    start inodes at 1; add a add_or_get() function

--- src/inodetable.rs
+++ src/inodetable.rs
@@ -6,6 +6,7 @@
 // for the duration of the mount. These are not persisted anywhere (on unmount, they go away).
 
 use std::collections::BTreeMap;
+use std::collections::btree_map::Entry::*;
 use std::ffi::OsString;
 use std::rc::Rc;
 
@@ -25,7 +26,7 @@ impl InodeTable {
     }
     
     pub fn add(&mut self, path: OsString) -> Inode {
-        let inode = self.table.len() as Inode;
+        let inode = (self.table.len() + 1) as Inode; // inodes will start at 1
         let rc = Rc::new(path);
         match self.map.insert(rc.clone(), inode) {
             Some(_) => { panic!("duplicate path inserted into inode table!"); },
@@ -34,15 +35,32 @@ impl InodeTable {
         self.table.push(rc);
         inode
     }
+
+    pub fn add_or_get(&mut self, path: Rc<OsString>) -> Inode {
+        match self.map.entry(path.clone()) {
+            Vacant(entry) => {
+                let inode = (self.table.len() + 1) as Inode;
+                entry.insert(inode);
+                self.table.push(path);
+                inode
+            },
+            Occupied(entry) => {
+                *entry.get()
+            }
+        }
+    }
     
-    pub fn get_path(&self, inode: Inode) -> Option<&OsString> {
-        match self.table.get(inode as usize) {
-            Some(rc) => Some(&rc),
+    pub fn get_path(&self, inode: Inode) -> Option<Rc<OsString>> {
+        match self.table.get((inode - 1) as usize) {
+            Some(rc) => Some(rc.clone()),
             None     => None
         }
     }
     
-    pub fn get_inode(&self, path: &OsString) -> Option<&Inode> {
-        self.map.get(path)
+    pub fn get_inode(&self, path: &OsString) -> Option<Inode> {
+        match self.map.get(path) {
+            Some(inode) => Some(*inode),
+            None        => None
+        }
     }
 }
